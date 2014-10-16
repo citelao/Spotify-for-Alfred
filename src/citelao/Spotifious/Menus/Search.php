cadbe8377346e46863b78b080e148df0457be1c6
Ben <ben@stolovitz.com>
2014-10-16 18:42:18 -0500

    new API for search

--- src/citelao/Spotifious/Menus/Search.php
+++ src/citelao/Spotifious/Menus/Search.php
@@ -18,58 +18,70 @@ class Search implements Menu {
 
 		$locale = $this->alfred->options('country');
 
+		/* Fetch and parse the search results. */
+		$urlQuery = str_replace("%3A", ":", urlencode($query));
+		$url = "https://api.spotify.com/v1/search?q=$urlQuery&type=artist,album,track&market=$locale";
+		
+		$fetcher = new JsonFetcher($url);
+		$json = $fetcher->run();
+
 		// Build the search results
 		// for each query type
 		foreach (array('artist', 'album', 'track') as $type) {
-			/* Fetch and parse the search results. */
-			$urlQuery = str_replace("%3A", ":", urlencode($query));
-			$url = "http://ws.spotify.com/search/1/$type.json?q=$urlQuery";
-			
-			$fetcher = new JsonFetcher($url);
-			$json = $fetcher->run();
-
-			// Create the search results array
-			foreach ($json->{$type . "s"} as $key => $value) {
-				// Determine region availability
-				if($type == "track") {
-					if(isset($value->album->availability)) {
-						$regions = $value->album->availability->territories;
-					} else {
-						$regions = "";
-					}
-				} else if($type == "album") {
-					if(isset($value->availability)) {
-						$regions = $value->availability->territories;
-					} else {
-						$regions = "";
-					}
-				} else {
-					$regions = "";
+			// Albums do not include artist data.
+			// Grab all the album ids, and find their artists
+			if($type == 'album') {
+				$albumIDs = array();
+				foreach ($json->albums->items as $key => $value) {
+					$albumIDs[] = $value->id;
 				}
 
-				if(mb_strlen($regions) > 0 && $this->contains($regions, $locale))
-					continue;
+				$urlQuery = str_replace("%3A", ":", urlencode(join(',', $albumIDs)));
+				$url = "https://api.spotify.com/v1/albums?ids=$urlQuery";
+				
+				$albumFetcher = new JsonFetcher($url);
+				$albumsJson = $albumFetcher->run();
 
+				$albums = array();
+				foreach ($albumsJson->albums as $key => $value) {
+					$albums[] = array(
+						'artist' => $value->artists[0]->name,
+						'popularity' => $value->popularity
+					);
+				}
+			}
+
+			// Create the search results array
+			foreach ($json->{$type . "s"}->items as $key => $value) {
 				// Weight popularity
-				$popularity = $value->popularity;
+				if($type == 'album') {
+					$popularity = $albums[$key]['popularity'];
+				} else {
+					$popularity = $value->popularity;
+				}
 
 				if($type == 'artist')
-					$popularity += .5;
+					$popularity += 50;
 
 				if($type == 'album')
-					$popularity += .15;
+					$popularity += 25;
 
 				if ($type == 'track') {
 					$currentRaw['album'] = $value->album->name;
 					$currentRaw['artist'] = $value->artists[0]->name;	
 				} elseif ($type == 'album') {
-					$currentRaw['artist'] = $value->artists[0]->name;
+					$currentRaw['artist'] = $albums[$key]['artist'];
 				}
 
-				$currentRaw['type'] = $type;
+				if($type == 'album') {
+					$currentRaw['type'] = $value->album_type;	
+				} else {
+					$currentRaw['type'] = $value->type;	
+				}
+				
 				$currentRaw['title'] = $value->name;
 				$currentRaw['popularity'] = $popularity;
-				$currentRaw['href'] = $value->href;
+				$currentRaw['uri'] = $value->uri;
 
 				$this->search[] = $currentRaw;
 			}
@@ -88,18 +100,20 @@ class Search implements Menu {
 					$subtitle = "$popularity {$current['album']} by {$current['artist']}";
 				} elseif ($current['type'] == 'album') {
 					$subtitle = "$popularity Album by {$current['artist']}";
+				} elseif ($current['type'] == 'single') {
+					$subtitle = "$popularity Single by {$current['artist']}";
 				} else {
 					$subtitle = "$popularity " . ucfirst($current['type']);
 				}
 
 				if ($current['type'] == 'track') {
 					$valid = 'yes';
-					$arg = "spotify⟩play track \"{$current['href']}\"";
+					$arg = "spotify⟩play track \"{$current['uri']}\"";
 					$autocomplete = '';
 				} else {
 					$valid = 'no';
 					$arg = '';
-					$autocomplete = "{$current['href']} ⟩ {$this->query} ⟩";
+					$autocomplete = "{$current['uri']} ⟩ {$this->query} ⟩";
 				}
 
 				$currentResult['title']    = $current['title'];
@@ -108,7 +122,7 @@ class Search implements Menu {
 				$currentResult['valid'] = $valid;
 				$currentResult['arg'] = $arg;
 				$currentResult['autocomplete'] = $autocomplete;
-				$currentResult['copy'] = $current['href'];
+				$currentResult['copy'] = $current['uri'];
 				$currentResult['icon'] = "include/images/{$current['type']}.png";
 
 				$results[] = $currentResult;
