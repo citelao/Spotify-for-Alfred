059f5a701eac80c1958d71595f2c275940aa0cd9
Ben <ben@stolovitz.com>
2017-11-24 17:11:42 -0500

    check for installed Spotify

--- src/citelao/Spotifious/Menus/Setup.php
+++ src/citelao/Spotifious/Menus/Setup.php
@@ -11,11 +11,13 @@ class Setup implements Menu {
 	protected $applicationCreated;
 	protected $applicationLinked;
 	protected $applicationPreviouslyLinked;
+	protected $hasInstalledSpotify;
 
 	public function __construct($query, $alfred, $api=null) {
 		$this->alfred = $alfred;
 
 		$this->wasOptOut = $this->alfred->options('spotify_app_opt_out') == 'true';
+		$this->hasInstalledSpotify = $this->alfred->options('has_installed_spotify') == 'true';
 
 		$this->countryCodeConfigured = !($this->alfred->options('country') == '');
 		$this->applicationCreated = !($this->alfred->options('spotify_client_id') == '' || $this->alfred->options('spotify_secret') == '');
@@ -45,7 +47,14 @@ class Setup implements Menu {
 		}
 
 		$results[] = array(
-			'title' => '1. Set your country code',
+			'title' => '1. Download & install Spotify',
+			'subtitle' => 'Spotifious only works with the Spotify desktop app.',
+			'icon' => array('path' => $this->hasInstalledSpotify ? 'include/images/checked.png' : 'include/images/unchecked.png'),
+			'arg' => '{"action":"command", "options": {"command": "open https://www.spotify.com/us/download/mac/"}}',
+		);
+
+		$results[] = array(
+			'title' => '2. Set your country code',
 			'subtitle' => 'Choosing the correct country code makes sure you can play songs you select.',
 			'icon' => array('path' => $this->countryCodeConfigured ? 'include/images/checked.png' : 'include/images/unchecked.png'),
 			'autocomplete' => 'Country Code ⟩',
@@ -53,7 +62,7 @@ class Setup implements Menu {
 		);
 
 		$results[] = array(
-			'title' => '2. Create a Spotify application',
+			'title' => '3. Create a Spotify application',
 			'subtitle' => 'Set up a Spotify application so you can search playlists!',
 			'icon' => array('path' => $this->applicationCreated ? 'include/images/checked.png' : 'include/images/unchecked.png'),
 			'arg' => 'appsetup⟩'
@@ -61,7 +70,7 @@ class Setup implements Menu {
 
 		if($this->applicationPreviouslyLinked) {
 			$results[] = array(
-				'title' => '3. Relink your Spotify application',
+				'title' => '4. Relink your Spotify application',
 				'subtitle' => "We've added new features to Spotifious, but you need to login again to use them.",
 				'icon' => array('path' => $this->applicationCreated ? $this->applicationLinked ? 'include/images/checked.png' : 'include/images/unchecked.png' : 'include/images/disabled.png'),
 				'arg' => 'applink⟩',
@@ -70,7 +79,7 @@ class Setup implements Menu {
 
 		} else {
 			$results[] = array(
-				'title' => '3. Link your Spotify application',
+				'title' => '4. Link your Spotify application',
 				'subtitle' => 'Connect your Spotify application to Spotifious to search your playlists.',
 				'icon' => array('path' => $this->applicationCreated ? $this->applicationLinked ? 'include/images/checked.png' : 'include/images/unchecked.png' : 'include/images/disabled.png'),
 				'arg' => 'applink⟩',
