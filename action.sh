3082a9e5aff5076563c311cd2103eb5166e42eff
Ben Stolovitz <ben@stolovitz.com>
2022-01-17 16:50:08 -0800

    avoid action when php not installed

--- action.sh
+++ action.sh
@@ -1,4 +1,10 @@
 # Add Homebrew to path for those who need :)
 export PATH="/usr/local/bin:${PATH}"
 
+# If no PHP installed, exit with no action.
+if ! command -v sphp &> /dev/null
+then
+	exit
+fi
+
 php -f action.php -- $1
